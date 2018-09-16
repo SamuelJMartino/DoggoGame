5cd2850b603d9d7520cede28bec032ab21f28100
Lexie26 <43228111+Lexie26@users.noreply.github.com>
2018-09-15 23:02:15 -0700

    :0 ?
    :100:

--- js.js
+++ js.js
@@ -1,32 +1,63 @@
-function dog(id, coat, nrg, eye){
-    this.id = id;
-    this.color = coat;
-    this.nrg = nrg;
-    this.eyeColor = eye;
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id="demo"></p>
+
+<script>
+
+//Things I'm trying to fix: [1] How it says "undefined" amongst my alleles when the code is ran [2] how to make it run 1 weighted random K allele instead of 3 at once like it is rn
+
+//go to https://www.w3schools.com/js/tryit.asp?filename=tryjs_ifthen 
+
+//use their java IDE to run this youll have an easier time than trying to do it through html and chrome like me, an idiot
+
+//also go to http://www.javascriptkit.com/javatutors/weighrandom2.shtml to understand what the weight_EeEe and pretty much everything new is. I'm using it to make certain Kk(black gene) alleles in my K array occur 50% more often than KK and kk like they naturally do when the parents are Kk and Kk like kenzie and rainbow are here. I want to eventually implement into that "if" statement that is commented out once the undefines are fixed.
+
+function dog(id, coat, nrg, eye, K_){
+this.id = id;
+this.color = coat;
+this.nrg = nrg;
+this.eyeColor = eye;
+this.K = K_;
 }
 
-var kenzie= new dog(1,"gold","energetic","brown");
-var max= new dog(2,"black","lazy","black");
+var K = ["KK", "Kk", "kk"] //these are the 3 alleles for the black gene in dogs
 
-function createPuppy(mom, dad) {
-    var eyeColors = [mom.eyeColor, dad.eyeColor];
-    var coatColors = [mom.color, dad.color];
-    var nrgLevels = [mom.nrg, dad.nrg];
+//var weight_EEEE= [4,0,0]
+//var weight_EEEe = [2,2,0]
+var weight_EeEe = [1,2,1] // <-- this is the only one that matters atm since the parents, Kenzie and rainbow, have the matching Kk/Kk (or KkKk) alleles as you can see below in their arrays. this is the weight_EeEe that I explained above. Its also explained by that link that I sent, except in fruit terms lol
+//var weight_Eeee = [0,2,2]
 
-    //Choose a color / eye color at random from the parents
-    this.eyeColor = eyeColors [Math.floor(Math.random() * eyeColors .length)];
-    this.color = coatColors [Math.floor(Math.random() * coatColors .length)];
+var kenzie = new dog(1,"gold","energetic","brown", "Kk");
+var rainbow= new dog(2,"black","lazy","black", "Kk");
 
-    //Get a random energy level between the parents levels.
-    this.nrg = Math.floor(Math.random() * Math.max(nrgLevels)) + Math.min(nrgLevels)  
+/*
+if (kenzie.K && rainbow.K == K[1]){
+window.alert("yay! The if statement for matching K alleles works.")
+} else {
+window.alert("boo!")
+}
+*/
+
+//Below is the rest of that tutorial for making Kk/Kk occur 2x more often than KK and kk. It seems to be working aside from the "undefined" that it keeps creating. And the fact that its creating 3 different weighted random alleles at once instead of 1 weighted random one. Wtf is causing that undefined-ness
+var totalweight=eval(weight_EeEe.join("+"))
+var weighed=new Array()
+var currentAllele=0
+
+while (currentAllele<K.length){ 
+    for (i=0; i<weight_EeEe[currentAllele]; i++)
+        weighed[weighed.length]=K[currentAllele]
+    currentAllele++
+    
+    var randomnumber=Math.floor(Math.random()*totalweight)
+	document.write(weighed[randomnumber])
+
+
 }
 
-//Then you will want to generate a puppy on a button press.
-//We can store these puppies in an array.
-var  puppies = [];
 
-//This $() uses jQuery. (http://api.jquery.com/on/) 
-// It allows you to execute a function when an even fires on the element.
-$('#some-button-id').on('click', function() { 
-    puppies.push(createPuppy(kenzie,max));
-});
+</script>
+
+</body>
+</html>
